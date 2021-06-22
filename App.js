aaa68aa42924e4927cad99333ca1b52cb7dd7d54
Alejandro <alextalin@gmail.com>
2021-06-22 21:07:13 +0200

    Testing API call

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  
+
+  useEffect(() => {
+    fetch('https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=6dc53e72-753d-4a84-a151-24fc135cd742')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+     
+  }, []);
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{ flex: 1, padding: 24 }}>
+    {isLoading ? <Text>Loading...</Text> : 
+    ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{data.records_format}</Text>
+        <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Articles:</Text>
+        <FlatList
+          data={data.result.records}
+          //keyExtractor={({ _id }, index) => _id}
+          renderItem={({ item }) => (
+            <Text>{item.NOMBRE}</Text>
+          )}
+        />
+      </View>
+    )}
+  </View>
   );
 }
 
