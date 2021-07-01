5590f104f77fe0d2734aaede1f7102c81043c824
Alejandro <alextalin@gmail.com>
2021-07-01 20:09:18 +0200

    Added category page

--- App.js
+++ App.js
@@ -7,11 +7,95 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 function PantallaInicio ({navigation}){
 
+  const LISTADOCAT = [
+    {
+      id: '0f3559d4-d16b-45f8-9859-e8baff32c6d5',
+      title: 'Deportes',
+    },
+    {
+      id: '1fdff041-b1ef-47f4-aa91-5a2522bf4498',
+      title: 'Colectivo',
+    },
+    {
+      id: '781a0645-6c63-46fc-be87-b81018bf8b55',
+      title: 'Congresos, conferencias y festivales',
+    },
+    {
+      id: '587b2815-4c8d-4bac-9442-c0c9147136fc',
+      title: 'Cursos y talleres',
+    },
+    {
+      id: '043036db-9a19-4a0f-91d5-732cfb2c1532',
+      title: 'Espectaculos',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Ferias, Exposiciones y Museos',
+    },
+    {
+      id: 'b3fb171d-a2d9-4266-b055-7acf206d9439',
+      title: 'Fiestas populares',
+    },
+    {
+      id: '9a6de506-6835-4636-80db-30f29bc1c6c1',
+      title: 'Hechos de Vida',
+    },
+    {
+      id: '1d497756-35b7-44d5-bc13-dd82e62f5ec0',
+      title: 'Música',
+    },
+    {
+      id: '1d497756-35b7-44d5-bc13-dd82e62f5ec1',
+      title: 'Perfiles',
+    },
+    {
+      id: '3de3132e-f727-431d-bd65-722d39f3aacf',
+      title: 'Premios y concursos',
+    },
+    {
+      id: 'be13814c-290d-43a1-b10f-4aeb31b774b8',
+      title: 'TemasWeb',
+    },
+    {
+      id: '18fa1e73-6b1a-4e42-b48c-3002b1374fac',
+      title: 'Viajes y excursiones',
+    },
+  ];
+   /* Maqueto un item del listado */
+   const renderItem = ({item, index}) => {
+    return(
+
+        <View style={styles.item}>
+          <View>
+            <Text style={styles.itemtitulo}>{item.title}</Text>
+          </View>
+          <View style={styles.itemcontainerbotones}>
+            <TouchableOpacity 
+            style={styles.botones}         
+            onPress={() => navigation.navigate('Categoría', item.title)}>
+            <Text style={styles.botonestexto}>Ver Actividades</Text></TouchableOpacity>
+          </View>
+        </View>
+
+       )
+
+  }
+
+
   return(
-    <Button
-    title="Test"
-    onPress={() => navigation.navigate('Categoría')}
-  />
+    
+  <View style={{ flex: 1, padding: 24 }}>
+    
+    <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>Todas las categorías</Text>
+        <FlatList
+          data={LISTADOCAT}
+          keyExtractor={item => item.id.toString()} //Debe ser un string
+          renderItem={renderItem}
+        />
+      </View>
+    
+  </View>
   );
 
 }
@@ -21,16 +105,19 @@ function PantallaCategorias({route, navigation }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const idCategoria  = route.params;
-  
+  //console.log(idCategoria);
 
   useEffect(() => {
-    fetch('https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=6dc53e72-753d-4a84-a151-24fc135cd742&q={"CATEGORIA":"Deportes"}')
+    fetch(`https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=6dc53e72-753d-4a84-a151-24fc135cd742&q={"CATEGORIA":"${idCategoria}"}`)
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
      
   }, []);
+  console.log(data.result.resource_id);
+ 
+  
 
    /* Maqueto un item del listado */
   const renderItem = ({item, index}) => {
@@ -62,7 +149,7 @@ function PantallaCategorias({route, navigation }) {
     <View style={{ flex: 1, padding: 24 }}>
     {isLoading ? <Text>Cargando...</Text> : 
     ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>Activadades</Text>
+        <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>{idCategoria}</Text>
         <FlatList
           data={data.result.records}
           keyExtractor={item => item._id.toString()} //Debe ser un string
