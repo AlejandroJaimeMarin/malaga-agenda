99f7443a41c56d8921dd67fb16d815c9b848d76e
Alejandro <alextalin@gmail.com>
2021-07-03 18:38:15 +0200

    Added activity page

--- App.js
+++ App.js
@@ -73,7 +73,8 @@ function PantallaInicio ({navigation}){
             <TouchableOpacity 
             style={styles.botones}         
             onPress={() => navigation.navigate('Categoría', item.title)}>
-            <Text style={styles.botonestexto}>Ver Actividades</Text></TouchableOpacity>
+            <Text style={styles.botonestexto}>Ver Actividades</Text>
+            </TouchableOpacity>
           </View>
         </View>
 
@@ -106,7 +107,8 @@ function PantallaCategorias({route, navigation }) {
   const [data, setData] = useState([]);
   const idCategoria  = route.params;
   //console.log(idCategoria);
-
+  //`https://datosabiertos.malaga.eu/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20"6dc53e72-753d-4a84-a151-24fc135cd742"%20WHERE%20"CATEGORIA"%20LIKE%20${idCategoria}`
+  //`https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=6dc53e72-753d-4a84-a151-24fc135cd742&q={"CATEGORIA":"${idCategoria}"}`
   useEffect(() => {
     fetch(`https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=6dc53e72-753d-4a84-a151-24fc135cd742&q={"CATEGORIA":"${idCategoria}"}`)
       .then((response) => response.json())
@@ -115,8 +117,7 @@ function PantallaCategorias({route, navigation }) {
       .finally(() => setLoading(false));
      
   }, []);
-  console.log(data);
- 
+  
   
 
    /* Maqueto un item del listado */
@@ -127,24 +128,26 @@ function PantallaCategorias({route, navigation }) {
             <View>
               <Text style={styles.itemtitulo}>{item.NOMBRE.replace(/(<([^>]+)>)/gi, "")}</Text>
               <Text>{item.DESCRIPCION.replace(/(<([^>]+)>)/gi, "")}</Text>
-              <Text style={styles.itemcategoria}>{item.CATEGORIA}</Text>
-              <Text>{item.F_INICIO}</Text>
-              <Text>{item.F_FIN}</Text>
-              
             </View>
             <View style={styles.itemcontainerbotones}>
-              <TouchableOpacity style={styles.botones} ><Text style={styles.botonestexto}>Saber más</Text></TouchableOpacity>
+              <TouchableOpacity 
+              style={styles.botones} 
+              onPress={() => navigation.navigate('Ficha', {titulo: item.NOMBRE, descripcion: item.DESCRIPCION, categoria: item.CATEGORIA, 
+              finicio: item.F_INICIO, ffinal: item.F_FIN, telefono: item.TELEFONO, email: item.E_MAIL, web: item.DIRECCION_WEB, horario: item.HORARIO, 
+              especialidad: item.ESPECIALIDAD, destinatarios: item.DESTINATARIOS_DESCRIPCION, direccion1: item.ID_LUGAR, direccion2: item.OTROS_LUGARES})}>
+                <Text style={styles.botonestexto}>Saber más</Text>
+              </TouchableOpacity>
             </View>
           </View>
 
          )
 
     }
-    var arr = [{"time":"2016-07-26 09:02:27","type":"aa"}, {"time":"2016-04-21 20:35:07","type":"ae"}, {"time":"2016-08-20 03:31:57","type":"ar"}, {"time":"2017-01-19 22:58:06","type":"ae"}, {"time":"2016-08-28 10:19:27","type":"ae"}, {"time":"2016-12-06 10:36:22","type":"ar"}, {"time":"2016-07-09 12:14:03","type":"ar"}, {"time":"2016-10-25 05:05:37","type":"ae"}, {"time":"2016-06-05 07:57:18","type":"ae"}, {"time":"2016-10-08 22:03:03","type":"aa"}, {"time":"2016-08-13 21:27:37","type":"ae"}, {"time":"2016-04-09 07:36:16","type":"ar"}, {"time":"2016-12-30 17:20:08","type":"aa"}, {"time":"2016-03-11 17:31:46","type":"aa"}, {"time":"2016-05-04 14:08:25","type":"ar"}, {"time":"2016-11-29 05:21:02","type":"ar"}, {"time":"2016-03-08 05:46:01","type":"ar"}, ];
-    var filtered = arr.filter(a => a.type == "ar");
+    //var arr = [{"time":"2016-07-26 09:02:27","type":"aa"}, {"time":"2016-04-21 20:35:07","type":"ae"}, {"time":"2016-08-20 03:31:57","type":"ar"}, {"time":"2017-01-19 22:58:06","type":"ae"}, {"time":"2016-08-28 10:19:27","type":"ae"}, {"time":"2016-12-06 10:36:22","type":"ar"}, {"time":"2016-07-09 12:14:03","type":"ar"}, {"time":"2016-10-25 05:05:37","type":"ae"}, {"time":"2016-06-05 07:57:18","type":"ae"}, {"time":"2016-10-08 22:03:03","type":"aa"}, {"time":"2016-08-13 21:27:37","type":"ae"}, {"time":"2016-04-09 07:36:16","type":"ar"}, {"time":"2016-12-30 17:20:08","type":"aa"}, {"time":"2016-03-11 17:31:46","type":"aa"}, {"time":"2016-05-04 14:08:25","type":"ar"}, {"time":"2016-11-29 05:21:02","type":"ar"}, {"time":"2016-03-08 05:46:01","type":"ar"}, ];
+   // var filtered = arr.filter(a => a.type == "ar");
     //console.log(data);
    //var filtered = data.filter(a => a.CATEGORIA == "Deportes");
-
+   //console.log(filtered)
   return (
     <View style={{ flex: 1, padding: 24 }}>
     {isLoading ? <Text>Cargando...</Text> : 
@@ -161,6 +164,28 @@ function PantallaCategorias({route, navigation }) {
   );
 }
 
+function PantallaFilla({route, navigation }){
+
+  const {titulo, descripcion, categoria, direccion1, direccion2, especialidad, destinatarios, finicio, ffinal, telefono, email, web, horario} = route.params;
+
+  return (
+    <View>
+      <Text>{titulo.replace(/(<([^>]+)>)/gi, "")}</Text>
+      <Text>{descripcion.replace(/(<([^>]+)>)/gi, "")}</Text>
+      <Text>{categoria}</Text>
+      <Text>{direccion1}</Text>
+      <Text>{direccion2}</Text>
+      <Text>{especialidad}</Text>
+      <Text>{destinatarios}</Text>
+      <Text>{finicio}</Text>
+      <Text>{ffinal}</Text>
+      <Text>{horario}</Text>
+      <Text>{telefono}</Text>
+      <Text>{email}</Text>
+      <Text>{web}</Text>
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
@@ -172,6 +197,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Inicio">
         <Stack.Screen name="Inicio" component={PantallaInicio} />
         <Stack.Screen name="Categoría" component={PantallaCategorias} />
+        <Stack.Screen name="Ficha" component={PantallaFilla}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
